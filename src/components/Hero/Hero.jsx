1c476246a6a2fd32a38b9b31364ca7896b0e9d83
Daniel Daineko <danieldaineko@eek.ee>
2026-05-09 11:14:13 +0300

    Add top logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,6 +12,9 @@ function Hero() {
 
   return (
     <section id="hero" className="hero reveal">
+      <div className="hero__top-logo">
+        <img src="/images/et-logo.png" alt="EuroTransfer logo" />
+      </div>
       <div className="hero__bg"></div>
       <div className="hero__overlay"></div>
 
